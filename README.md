8180cf43ff3f1f41fb82c59fdf9de53cab91c0b6
Reis25 <demetriosreis1@gmail.com>
2022-05-22 00:33:03 -0300

    retornando json com os dados dos hoteis

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# <img src="https://img.shields.io/badge/Udemy-A100FF?style=for-the-badge&logo=Udemy&logoColor=white" />  Curso de: REST API com Python e Flask 
+# <img src="https://img.shields.io/badge/Udemy-A100FF?style=for-the-badge&logo=Udemy&logoColor=white" /> : REST API com Python e Flask 
 
 
 
--- app.py
+++ app.py
@@ -2,15 +2,39 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
-Api = Api(app)
+api = Api(app)
+
+lista_hoteis = [
+    {
+        'hotel_id': 1,
+        'nome': "alpha",
+        'avaliacao': 5,
+        'cidade': 'RJ',
+        'diaria': 500
+    }, 
+    {
+        'hotel_id': 2,
+        'nome': "Beta",
+        'avaliacao': 4,
+        'cidade': 'SE',
+        'diaria': 400
+    },
+    {
+        'hotel_id': 3,
+        'nome': "gama",
+        'avaliacao': 3,
+        'cidade': 'AL',
+        'diaria': 300
+    }
+]
 
 class Hoteis(Resource):
     
     def get(self):
-        return {'hoteis': 'meus hoteis'}
+        return {'hoteis': lista_hoteis}
     
-api.add_resource(Hoteis, '/hoteis')
+api.add_resource(Hoteis, '/nome_hoteis')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) #enquanto estivermos desenvolvendo. 
 
